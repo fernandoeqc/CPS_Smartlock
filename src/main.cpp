7cb7776cc026e5bedcd751db66852391cb0ec089
fernando <fernandoeqc@live.com>
2024-11-18 18:53:38 -0300

    add tasks

--- src/main.cpp
+++ src/main.cpp
@@ -18,29 +18,46 @@ Doorman doorman(BUZZER_PIN, LOCK_PIN);
 
 Card_Manager cm(SS_PIN, RST_PIN);
 
+void loop_card(void *z) {
+    while(true) {
+        cm.listen();
+        String card_id = cm.read();
+
+        int card_level = 2;
+
+        if (cm.is_card_registred(card_id)) {
+            Card* card = cm.get(card_id);
+            card_level = card->get_level();
+        }
+
+        Serial.println(card_level);
+
+        doorman.access(card_level);
+
+        delay(200);
+        Serial.println("next loop");
+
+    }
+}
+
+void loop_mqtt(void *z) {
+    while(true) {
+        delay(1000);
+        Serial.println("Mqtt a implementar");
+    }
+}
 void setup() {
     Serial.begin(115200);
     doorman.init();
     cm.init();
     cm.append(white_card);
     cm.append(key_chain);
+    xTaskCreate(loop_mqtt, "loop_mqtt", 2048, NULL, 1, NULL);
+    xTaskCreate(loop_card, "loop_mqtt", 2048, NULL, 1, NULL);
+
 }
 
 void loop() {
-    cm.listen();
-    String card_id = cm.read();
+    delay(1000);
 
-    int card_level = 2;
-
-    if (cm.is_card_registred(card_id)) {
-        Card* card = cm.get(card_id);
-        card_level = card->get_level();
-    }
-
-    Serial.println(card_level);
-
-    doorman.access(card_level);
-
-    delay(200);
-    Serial.println("next loop");
 }
